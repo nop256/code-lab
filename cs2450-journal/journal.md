c136b539d90fbc2f1758ed1304581e35b06a7d30
nop256 <N0P256@proton.me>
2025-09-03 19:42:59 -0600

    created exclusive cs2450 journal

---
+++ cs2450-journal/journal.md
@@ -0,0 +1,5 @@
+CS-2450 Semester Journal
+
+========================================
+Date: 2025-09-03 19:41:50
+Subject: Idea Storming: We gathered as a group to brainstorm ideas for our `youface` app. Many ideas were pitched, we decided on a pirate/wizard social media sort-of site.
---
+++ cs2450-journal/write.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import time, os
+
+
+def trackProgress():
+    """Tracks progress and stores it in a local text file."""
+    scriptdir = os.path.dirname(os.path.abspath(__file__)) #retrieving the absolute path of the directory the script is stored in
+    #print(scriptDir) # for testing
+    filename = os.path.join(scriptdir, "journal.md") #joining the absolute directory of script with filename
+    
+    #create the file if it doesn't exist
+    if not os.path.exists(filename): #if filename does not exist in this scripts directory:
+        with open(filename, "w", encoding="utf-8") as file: 
+            file.write("CS-2450 Semester Journal\n") #writing the title of the file
+        
+    #prompt user for today's progress
+    print("\nWhat entry would you like to log today?") #instruction
+    progress_entry = input(">>> ").strip() #prompts the user for their daily progress update
+
+    #capture timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #Year-Month-Day Hour:Minute:Second
+
+    with open(filename, "a", encoding="utf-8") as file: #utf encoding circumvents bugs I was having
+        file.write("\n" + "="*40 + "\n") #write a line break using equals sign
+        file.write(f"Date: {timestamp}\n") #record the current date and timestamp
+        file.write(f"Subject: {progress_entry}\n") #record the user's daily progress input from above
+
+    print("✅ Journal logged successfully!") #report log was recorded successfully
+    
+if __name__ == "__main__":
+    trackProgress()
+
